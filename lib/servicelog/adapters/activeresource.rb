3da5fd02276141bd5b177b0b3dab7d07aa0663ee
Dan Leyden <dan@52degreesnorth.com>
2020-11-27 17:27:03 +0000

    Ensure base classes are required before monkeypatching them
    When using adapters, if the monkeypatch is applied before the apdated
    class, the original implementation will be used. To prevent that from
    happening, require the base class before applying the monkeypatch.

--- lib/servicelog/adapters/activeresource.rb
+++ lib/servicelog/adapters/activeresource.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# Ensure that the base code has been loaded before monkeypatching
+# If it isn't loaded first, it will overwrite this monkeypatch
+require 'activeresource'
+
 module ActiveResource
   class Connection
     alias original_build_request_headers build_request_headers
--- lib/servicelog/adapters/httparty.rb
+++ lib/servicelog/adapters/httparty.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# Ensure that the base code has been loaded before monkeypatching
+# If it isn't loaded first, it will overwrite this monkeypatch
+require 'httparty'
+
 module HTTParty
   module ClassMethods
     alias original_headers headers
